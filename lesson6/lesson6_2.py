aa02f2e4c630dec3344063d96c1f5b2ccb5449f5
nettlefish-Dave <dchangs4g2@gmail.com>
2025-05-15 13:54:26 +0000

    lesson6_2_3

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -1,14 +1,19 @@
-#import tools
-#from tools import calc_bmi,check_bmi
-import edu
+#import tools   #module
+#from tools import calc_bmi,check_bmi   #module
+#import edu  #package
+#from edu import tools
+#from edu.tools import calc_bmi as a
+#from edu.tools import check_bmi as b
+from edu.tools import calc_bmi,check_bmi
 
 def main() :
     try:
         height:int = int(input('請輸入你的身高(公分 cm):'))
         weight = eval(input('請輸入你的體重(公斤 kg):'))        
-        #bmicalculate = tools.calc_bmi(height,weight)
-        #bmicalculate = calc_bmi(height,weight)
-        bmicalculate = edu.tools.calc_bmi(height,weight)
+        #bmicalculate = tools.calc_bmi(height,weight)   #module
+        #bmicalculate = calc_bmi(height,weight)     #module
+        #bmicalculate = edu.tools.calc_bmi(height,weight)    #package
+        bmicalculate = calc_bmi(height,weight)     #package
     except ValueError:
         print('輸入發生錯誤')
     except Exception as e :
@@ -17,9 +22,10 @@ def main() :
         print(f'你的身高:{height} 公分')
         print(f'你的體重:{weight:.2f} 公斤')
         print(f'你的 BMI值為:{bmicalculate:.2f}')
-        #print(tools.check_bmi(bmicalculate))
-        #print(check_bmi(bmicalculate))
-        print(edu.tools.check_bmi(bmicalculate))
+        #print(tools.check_bmi(bmicalculate))   #module
+        #print(check_bmi(bmicalculate))     #module
+        #print(edu.tools.check_bmi(bmicalculate))    #package
+        print(check_bmi(bmicalculate))     #package
 
 if __name__ == '__main__':
     main()
