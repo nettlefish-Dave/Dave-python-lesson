fe364ea7e2c85c41ea1017e43dccea945d1fa4be
nettlefish-Dave <dchangs4g2@gmail.com>
2025-05-15 13:44:26 +0000

    lesson6_2_3

---
+++ lesson6/edu/tools.py
@@ -0,0 +1,21 @@
+def calc_bmi(hei:int,wei:int)->float: #指定型態
+    if hei < 120 or hei > 220:
+        raise Exception(f'輸入的身高: {hei} 公分 不在 120 ~ 220 範圍內')
+    hei /= 100
+    if wei < 30.0 or wei > 200.0:
+        raise Exception(f'輸入的體重: {wei:.2f} 公斤 不在 30 ~ 200 範圍內')
+    return wei / hei ** 2
+
+def check_bmi(bmi:float)->str:
+    if bmi < 18.5:
+        return '你的體重過輕'
+    elif bmi < 24:
+        return '你的體重正常'
+    elif bmi < 27:
+        return '你的體重稍微過重'
+    elif bmi < 30:
+        return '你已經輕度肥胖'
+    elif bmi < 35:
+        return '你已經中度肥胖'
+    else:
+        return '你已經重度肥胖'
--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -1,22 +1,25 @@
 #import tools
-from tools import calc_bmi,check_bmi
+#from tools import calc_bmi,check_bmi
+import edu
 
 def main() :
     try:
         height:int = int(input('請輸入你的身高(公分 cm):'))
         weight = eval(input('請輸入你的體重(公斤 kg):'))        
         #bmicalculate = tools.calc_bmi(height,weight)
-        bmicalculate = calc_bmi(height,weight)
+        #bmicalculate = calc_bmi(height,weight)
+        bmicalculate = edu.tools.calc_bmi(height,weight)
     except ValueError:
         print('輸入發生錯誤')
     except Exception as e :
         print(e)
     else:
-        print(f'你的身高:{height*100} 公分')
+        print(f'你的身高:{height} 公分')
         print(f'你的體重:{weight:.2f} 公斤')
         print(f'你的 BMI值為:{bmicalculate:.2f}')
         #print(tools.check_bmi(bmicalculate))
-        print(check_bmi(bmicalculate))
+        #print(check_bmi(bmicalculate))
+        print(edu.tools.check_bmi(bmicalculate))
 
 if __name__ == '__main__':
     main()
