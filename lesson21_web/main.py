45a39498a8d19b71e506e710788948baa8fc77a3
nettlefish <dchang077@gmail.com>
2025-07-10 20:03:42 +0800

    modify

--- lesson21_web/main.py
+++ lesson21_web/main.py
@@ -3,16 +3,16 @@ from markupsafe import escape
 from google import genai
 
 app = Flask(__name__)
+client = genai.Client()
 
 @app.route("/")
 @app.route("/<string:question>")
 def index(question:str=''):
     if question == '':
-        return '<hl>這是Gemini畫面</hl>'
-    else:
-        client = genai.Client()
+        return "<hl>這是Gemini畫面</hl>"
+    else:        
         response = client.models.generate_content(
-            model="gemini-2.5-flash", contents=f"{question},回答請輸出為html格式"
+            model="gemini-2.5-flash", contents=f"{question},請輸出為html格式"
         )
         html_format = response.text
         html_format = response.text.replace('```html,""').replace('```,""')
